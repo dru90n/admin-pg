4d1254b9632f79b73a43954c4001da75fd57b0f7
dru90n <166186661+dru90n@users.noreply.github.com>
2025-06-30 12:55:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// Ganti dengan URL dan KEY kamu
-const SUPABASE_URL = "https://xxxx.supabase.co";
-const SUPABASE_KEY = "eyJhbGci..."; 
+// Inisialisasi Supabase
+const SUPABASE_URL = "https://zbunmfsedqalvvadwgkk.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpidW5tZnNlZHFhbHZ2YWR3Z2trIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNjE2NTUsImV4cCI6MjA2NjgzNzY1NX0.wUEGv5FavNSQT3iNlo6WnW_d3TcDVxRTx8sI6xD-wxQ";
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
